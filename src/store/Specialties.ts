b6c9fcab7d996b70e544bf104230b0c79ebf439b
igrisdev <johanalvarezz237@gmail.com>
2024-07-09 19:09:23 -0500

    update the project for used nanostores

--- src/store/Specialties.ts
+++ src/store/Specialties.ts
@@ -1,5 +1,8 @@
 import { atom } from 'nanostores'
 
+import SalchipapaSencilla from '@assets/imagesSalchipapas/salchipapa-original.webp'
+import HamburguesaSencilla from '@assets/imagesHamburguesas/hamburguesa-sencilla.webp'
+
 export const $isOpen = atom(false)
 
 interface ListaEspecialidades {
@@ -7,6 +10,22 @@ interface ListaEspecialidades {
   name: string
   href: string
 }
+
+/* interface Product {
+  id: number
+  name: string
+  description: string
+  image: string
+  price: string
+}
+
+interface Specialty {
+  [key: string]: Product[]
+}
+
+interface Specialties {
+  [index: number]: Specialty
+} */
 
 export const $listaEspecialidades = atom<ListaEspecialidades[]>([
   {
@@ -39,3 +58,46 @@ export const $listaEspecialidades = atom<ListaEspecialidades[]>([
 export function addListaEspecialidades(listaEspecialidad: ListaEspecialidades) {
   $listaEspecialidades.set([...$listaEspecialidades.get(), listaEspecialidad])
 }
+
+export const $specialties = atom([
+  {
+    salchipapas: [
+      {
+        id: 1,
+        name: 'Salchipapa Sencilla',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image: SalchipapaSencilla.src,
+        price: '6.000 ',
+      },
+      {
+        id: 2,
+        name: 'Salchipapa Sencilla',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image: SalchipapaSencilla.src,
+        price: '34.000',
+      },
+    ],
+  },
+  {
+    hamburguesas: [
+      {
+        id: 1,
+        name: 'Hamburguesa Sencilla',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image: HamburguesaSencilla.src,
+        price: '6.000',
+      },
+      {
+        id: 2,
+        name: 'Hamburguesa Grande',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image: HamburguesaSencilla.src,
+        price: '34.000',
+      },
+    ],
+  },
+])
