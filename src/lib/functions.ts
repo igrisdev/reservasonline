9b8588aff6b0a65f83b88e8ef89c67749c04a4f9
igrisdev <johanalvarezz237@gmail.com>
2024-07-30 15:50:44 -0500

    delete item when the quantity is 0

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -23,6 +23,15 @@ export const resSpecialtyShoppingCartStore = ({
   shoppingCartStore: ShoppingCartStoreInterface[]
   id: string
 }) => {
+  const isQuantityZero = shoppingCartStore.find(
+    (item: ShoppingCartStoreInterface) => item.quantity === 1
+  )
+
+  if (isQuantityZero)
+    return shoppingCartStore.filter(
+      (item: ShoppingCartStoreInterface) => item.quantity > 1
+    )
+
   return shoppingCartStore.map((item: ShoppingCartStoreInterface) => {
     if (item.id === id && item.quantity > 0) {
       return { ...item, quantity: item.quantity - 1 }
